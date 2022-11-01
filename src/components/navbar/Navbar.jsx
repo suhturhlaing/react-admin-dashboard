e40d100082e7f73379a77fd7be27d46093eb6fb4
Su Htur Hlaing <shh.zotefamily@gmail.com>
2022-11-01 11:41:06 +0630

    [modify] first commit

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,59 @@
 import "./navbar.scss"
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import LanguageOutlinedIcon from '@mui/icons-material/LanguageOutlined';
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import FullscreenExitOutlinedIcon from "@mui/icons-material/FullscreenExitOutlined";
+import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
+import ChatBubbleOutlineOutlinedIcon from "@mui/icons-material/ChatBubbleOutlineOutlined";
+import ListOutlinedIcon from "@mui/icons-material/ListOutlined";
+
 
 function Navbar() {
   return (
     <div className='navbar'>
-      Navbar
+      <div className="wrapper">
+
+        {/* Search */}
+        <div className="search">
+          <input type="text" placeholder="Search ...." />
+          <SearchOutlinedIcon />
+        </div> 
+
+        {/* Right Material icon option */}
+        <div className="items">
+          <div className="item">
+            <LanguageOutlinedIcon className="icon"/>
+            English
+          </div>
+
+          <div className="item">
+            <DarkModeOutlinedIcon className="icon"/>
+          </div>
+
+          <div className="item">
+            <FullscreenExitOutlinedIcon className="icon"/>
+          </div>
+
+          <div className="item">
+            <NotificationsNoneOutlinedIcon className="icon"/>
+            <div className="counter">1</div>
+          </div>
+
+          <div className="item">
+            <ChatBubbleOutlineOutlinedIcon className="icon"/>
+            <div className="counter">2</div>
+          </div>
+
+          <div className="item">
+            <ListOutlinedIcon className="icon"/>
+          </div>
+
+          <div className="item">
+            <img src="https://images.pexels.com/photos/941693/pexels-photo-941693.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" className="avatar" />
+          </div>
+        </div>
+
+      </div>
     </div>
   )
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Sidebar from '../../components/sidebar/Sidebar'
+import Navbar from '../../components/navbar/Navbar'
 import './home.scss'
 
 const Home = () => {
   return (
     <div className='home'>
       <Sidebar />
-      <div className='homeContainer'>container</div>
+      <div className='homeContainer'>
+        <Navbar />
+        container
+      </div>
     </div>
   )
 }
