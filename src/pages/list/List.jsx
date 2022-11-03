eff1f17e362b589f0aea0cb8331e66b0734e41fb
Su Htur Hlaing <shh.zotefamily@gmail.com>
2022-11-03 16:22:30 +0630

    [modify]create datatable with react data-grid

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import "./list.scss"
+import Navbar from '../../components/navbar/Navbar'
+import Sidebar from '../../components/sidebar/Sidebar'
+import Datatable from "../../components/datatable/Datatable"
 
 function List() {
   return (
-    <div>
-      this is List
+    <div className='list'>
+      <Sidebar />
+      <div className='listContainer'>
+        <Navbar />
+        <Datatable />
+      </div>
     </div>
   )
 }
